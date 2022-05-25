9e75ad96ce49cd96680a0dbf94ec9fff41a76a2c
李玺 <45314745+lixi5338619@users.noreply.github.com>
2022-05-25 19:55:12 +0800

    Create t1.js

---
+++ 第三章：Web Js逆向/3.7.0 浏览器环境补充/t1.js
@@ -0,0 +1,44 @@
+// 最基础的
+var glb;
+var window = global;
+window.document = {referrer: ""};
+window.location = {
+    hash: "",
+    host: "",
+    hostname: "",
+    href: "",
+    origin: "",
+    pathname: "/",
+    port: "",
+    protocol: "https:",
+    search: ""
+};
+window.navigator = {
+    appCodeName: "Mozilla",
+    appName: "Netscape",
+    appVersion: "5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36",
+    cookieEnabled: true,
+    deviceMemory: 8,
+    doNotTrack: null,
+    hardwareConcurrency: 4,
+    language: "zh-CN",
+    languages: ["zh-CN", "zh"],
+    maxTouchPoints: 0,
+    onLine: true,
+    platform: "Win32",
+    product: "Gecko",
+    productSub: "20030107",
+    userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36",
+    vendor: "Google Inc.",
+    vendorSub: "",
+};
+
+function get_signature(url) {
+    _signature = "";
+    return _signature;
+}
+
+// Js代码位置
+
+url = '';
+console.log(get_signature(url));
