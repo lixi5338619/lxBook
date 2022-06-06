417c1523bf1cfae344f5917da1b5c65e487b34c1
李玺 <45314745+lixi5338619@users.noreply.github.com>
2022-06-06 17:50:06 +0800

    Update client.js

--- 第三章：Web Js逆向/3.9.2 抖音直播数据RPC/client.js
+++ 第三章：Web Js逆向/3.9.2 抖音直播数据RPC/client.js
@@ -1,3 +1,24 @@
+// 这是当前新版的
+window.dataLx = s.toObject();
+!function(){
+            var res = window.dataLx;
+            if (window.flagLX){
+                    window.wsLX.send(JSON.stringify(res));
+            } 
+            else{
+                    var ws = new WebSocket("ws://127.0.0.1:9999");
+                    window.wsLX = ws;
+                    window.flagLX = true;
+                    ws.open = function(evt){};
+                    ws.onmessage = function(evt){
+                        ws.send(JSON.stringify(res));
+                    }
+            }
+}();
+
+
+
+// 这是视频中老版本的
 window.dataLx = r;                                                                                                                               
 !function(){
    var res = window.dataLx;
