6c8030fe3191c0f0b6a89314762d6b37af2a984c
李玺 <45314745+lixi5338619@users.noreply.github.com>
2022-07-27 09:26:29 +0800

    Update run.py

--- 第三章：Web Js逆向/3.6 加密参数还原与模拟/3.6.7 cnvd加速乐分析案例/run.py
+++ 第三章：Web Js逆向/3.6 加密参数还原与模拟/3.6.7 cnvd加速乐分析案例/run.py
@@ -7,6 +7,7 @@ import re
 import execjs
 
 headers = {
+    # 如果被拦截，把headers补齐
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36'
 
 }
